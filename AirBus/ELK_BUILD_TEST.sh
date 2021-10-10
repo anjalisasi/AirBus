0bf3c87262b6604d68e16ac083f158741b5e7ff5
root <root@4be46709091c.mylabserver.com>
2021-10-10 22:08:11 +0000

    test8

--- AirBus/ELK_BUILD_TEST.sh
+++ AirBus/ELK_BUILD_TEST.sh
@@ -6,6 +6,5 @@ scp DeleteAll.sh  ansible@$vault:/home/ansible
 ssh ansible@$vault chmod 700 /home/ansible/DeleteAll.sh
 ssh ansible@$vault /home/ansible/DeleteAll.sh
 ./Step3_Elkstack.sh
-./Step4_Es_UnitTest.sh
 ./Step4_Es_PerfTest.sh
 
